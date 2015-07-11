dd192036a05b23d2e001325398e61aaf82cce1fc
Rompue <Rompue@gmail.com>
2015-07-11 22:21:25 +0800

    last try

--- plot2.R
+++ plot2.R
@@ -17,12 +17,11 @@ rownames(newData) <- 1:nrow(newData)
 dim(newData) # 2880   10
 attach(newData)
 source("load_data.R")
-png(filename = "plot1.png", 
-    width = 480, height = 480, 
-    units = "px", bg = "transparent")
-hist(Global_active_power, 
-     col = "red", 
-     main = "Global Active Power", 
-     xlab = "Global Active Power (kilowatts)",
-     breaks = 12, ylim = c(0, 1200))
+png(filename = "plot2.png", 
+     width = 480, height = 480,
+     units = "px", bg = "transparent")
+plot(DateTime, Global_active_power, 
+     type = "l",
+     xlab = "",
+     ylab = "Global Active Power (kilowatts)")
 dev.off()
